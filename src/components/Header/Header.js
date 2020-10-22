59b3a75f47ea081fad8f6bfa448aa27dda2500ee
JEH_MACMINI <JEH@JEH-MACMINI.local>
2020-10-22 22:57:44 +0800

    test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import classes from './Header.module.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+
+    const mtClasses = useStyles();
+
+    return (
+        <div className={classes.HeaderBar}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={mtClasses.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={mtClasses.title}>
+                        Bareiss Service Web
+          </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Snackbar from '@material-ui/core/Snackbar';
+
+import classes from './Main.module.css';
+
+const Main = () => {
+
+    return(
+        <div className={classes.MainContent}>
+            
+        </div>
+    )
+}
+
+export default Main;
